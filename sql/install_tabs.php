26ecbab3e4f8809c9cf268d692ab9dbfbcd77d53
Konstantinos A. Kogkalidis <info@tapanda.gr>
2018-12-14 18:56:48 +0200

    Create install_tabs.php

---
+++ sql/install_tabs.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author     Konstantinos A. Kogkalidis <konstantinos@tapanda.gr>
+ * @copyright  2018 tapanda.gr <https://tapanda.gr/el/>
+ * @license    Single website per license
+ * @version    0.0.1
+ * @since      0.0.1
+ */
+
+$tabs = [
+	[
+        'class_name' => 'AdminFrameworkFiles',
+        'name' => 'Αρχεία',
+    ],[
+        'class_name' => 'AdminFrameworkHooks',
+        'name' => 'Άγκιστρα',
+    ],[
+        'class_name' => 'AdminFrameworkEntities',
+        'name' => 'Οντότητες',
+    ],[
+        'class_name' => 'AdminFrameworkSettings',
+        'name' => 'Ρυθμίσεις',
+    ]
+];
